d959ba8f2d9bd04f312c2d130d1a880f34c03fe7
awatanabe <awatanabe55@gmail.com>
2013-01-09 20:40:17 -0500

    Achieved "hai world"

---
+++ application/controllers/external.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * public.php
+ * UC Ficom - January 2013
+ * Aaron Watanabe - awatanabe@college.harvard.edu
+ * 
+ * Controller for publically accessable pages of FiCom Portal
+ */
+
+/**
+ * Description of public
+ *
+ * @author aaronwatanabe
+ */
+class External extends UC_Controller {
+
+    public function index(){
+        echo "o hai world";
+    }
+}
+?>
---
+++ application/core/UC_Controller.php
@@ -0,0 +1,23 @@
+<?php
+
+/***
+ * UC_Controller.php
+ * January 2013
+ * Aaron Watanabe - awatanabe@college.harvard.edu
+ * 
+ * Basic controller class for FiCom portal 
+ */
+
+/**
+ * Description of UC_Controller
+ *
+ * @author aaronwatanabe
+ */
+class UC_Controller extends CI_Controller {
+    
+    public function __construct (){
+        parent::__construct();
+    }
+}
+
+?>

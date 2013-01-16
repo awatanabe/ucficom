83b78a11c951660639f87df41fc29fed0ae7c06a
awatanabe <awatanabe55@gmail.com>
2013-01-15 23:08:41 -0500

    Navigation bar is working

--- application/views/universal/navigation.php
+++ application/views/universal/navigation.php
@@ -1,7 +1,12 @@
 <div class="grids">
     <div class="grid-16" id="navigation_bar">
         <ul id="navigation">
-            <li class="navigation_button">TODO<li>
+            <li class="navigation_button">
+                <?= anchor("external/index", "Home") ?>
+            </li>
+            <li class="navigation_button">
+                <?= anchor("external/about", "About") ?>
+            </li>                
         </ul>        
     </div>
 </div>

11a200bd3f5ad838643873b41faa7f3e800abf32
awatanabe <awatanabe55@gmail.com>
2013-01-16 23:27:08 -0500

    Updated table form for user edit

--- application/views/content/admin/edit_user.php
+++ application/views/content/admin/edit_user.php
@@ -1,7 +1,20 @@
-Edit User
-<br>
 <?= anchor("admin/index", "Return to admin page") ?>
-<br>
-<?= $edit_form ?>
-<br>
-<?= button("admin/deactivate_user/$user_id", "Delete User", "critical_button");
+<h2>Edit User</h2>
+<div class="grids">
+    <?= empty_grid(1) ?>
+    <div class="grid-5">
+        <p>
+            Update a user's information. You may reset a user's password with the reset password 
+            button, which will email the user will a new, random password (feature to be implemented).
+            You may also deactivate this user's account.
+        </p>
+        <div class="center_align">
+        <?= button("admin/deactivate_user/".$default_values[USERS_USER_ID],
+                        "Deactivate User", "critical_button") ?>        
+        </div>
+    </div>
+    <?= empty_grid(1) ?>
+    <div class="grid-9">
+        <?= $edit_form ?>
+    </div>
+</div>
--- application/views/universal/banner.php
+++ application/views/universal/banner.php
@@ -1,13 +1,13 @@
 <div class="grids">
-    <div class="grid-2" id="logo">
+    <div class="grid-3" id="logo">
         <a href="<?= $home_url?>">
             Logo
         </a>
     </div>
-    <div class="grid-12 highlight" id="masthead">
+    <div class="grid-10 highlight" id="masthead">
         Harvard UC Finance Committee
     </div>
-    <div class="grid-2" id="access">
+    <div class="grid-3" id="access">
         <?= $action ?>
     </div>
 </div>
